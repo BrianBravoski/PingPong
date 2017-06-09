52d5edcf311e7805d6144806a39340059bc6037e
BrianBravoski <Brianbravoski28@gmail.com>
2017-06-09 15:06:35 +0300

    Javascript added and Header aligned

--- js/pingpong.js
+++ js/pingpong.js
@@ -1,23 +1,27 @@
 // business logic
-var output = function(input) {
-  if (input % 3 === 0) {
-    alert("ping");
-  } else if (input % 5 === 0) {
-    alert("pong");
-  }else if ((input %5===0)&&(input% 3 ===0)) {
-    alert("pingpong");
-  }else{
-    alert(input);
+var output=[];
+var number = function(input){
+  for(var i=1; i<= input; i++){
+    if ((i%3 === 0)&&(i% 5=== 0)){
+      output.push("pingpong");
+    } else if (i% 5 === 0){
+      output.push("pong");
+    }else if(i% 3 === 0)
+    output.push("ping");
+  }else {
+    output.push(i);
   }
-}
-
+};
+};
 
 // user interface
 $(document).ready(function() {
   $(".form").submit(function(event){
     event.preventDefault();
-    var input = parseInt($("#input").val());
-    var result = output(input);
 
+    var input = parseInt($("#input").val());
+    var result = number(input);
+
+    $(".result").text(output);
   });
 });
